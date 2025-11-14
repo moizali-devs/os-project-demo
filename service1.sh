c1671e652ee8313c12b4edb25b7933ad22f965d4
Moiz Ali <moizpriv47@gmail.com>
2025-11-14 22:18:53 +0500

    Reverting to V1

--- service1.sh
+++ service1.sh
@@ -3,6 +3,6 @@
 LOG_FILE="/home/ubuntu/os-project-demo/service1.log"
 
 while true; do
-  echo "$(date) - Service 1 running - VERSION=v2" >> "$LOG_FILE"
+  echo "$(date) - Service 1 running - VERSION=v1" >> "$LOG_FILE"
   sleep 5
 done
--- service3.sh
+++ service3.sh
@@ -3,6 +3,6 @@
 LOG_FILE="/home/ubuntu/os-project-demo/service3.log"
 
 while true; do
-  echo "$(date) - Service 3 running - VERSION=v2" >> "$LOG_FILE"
+  echo "$(date) - Service 3 running - VERSION=v1" >> "$LOG_FILE"
   sleep 5
 done
